81b71015a1f138ba667a4488628bcca5b3f31811
Magnus <beachmagnus+git@gmail.com>
2020-12-09 14:19:24 +0100

    header styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import HomePage from './pages/homepage/HomePage.component';
-import ShopPage from './pages/shop/shop.component';
+
+import "./App.css";
 
 import {Switch, Route} from "react-router-dom";
+import HomePage from './pages/homepage/HomePage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/shop" component={ShopPage} />
