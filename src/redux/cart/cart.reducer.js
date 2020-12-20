271eabb033c892c4dbbd15b7b467900d7786bac8
Magnus <beachmagnus+git@gmail.com>
2020-12-20 22:26:18 +0100

    added an additem array, added a redux reducer for adding items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,21 +1,27 @@
 import CartActionTypes from './cart.types'
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type.TOGGLE_CART_HIDDEN) {
+    switch(action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN: 
             return {
                 ...state,
                 hidden: !state.hidden
             }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state, 
+                cartItems: [...state.cartItems, action.payload]
+            }
         default: 
         // ** DEBUGGING:
-        // console.log("CartActionTypes.TOGGLE_CART_HIDDEN er ")
-        // console.log(CartActionTypes.TOGGLE_CART_HIDDEN)
-        // console.log(action.type)
+        console.log("CartActionTypes.TOGGLE_CART_HIDDEN:")
+        console.log(CartActionTypes.TOGGLE_CART_HIDDEN)
+        console.log(action.type)
         return state;
     }
 }
