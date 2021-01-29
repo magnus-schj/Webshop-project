76b6493ef412631b1f8e9e811feea9376b827141
Magnus <beachmagnus+git@gmail.com>
2021-01-29 12:47:23 +0100

    added tghe shop data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,22 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from "../../redux/shop/shop.selectors";
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map((
+      { id, ...otherCollectionProps } //OTHER PROPS ARE ITEM, TITLE, AND ROUTENAME
+    ) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-  render() {
-    const { collections } = this.state;
-    console.log(collections);
-    return (
-      <div className="shop-page">
-        {collections.map((
-          { id, ...otherCollectionProps } //OTHER PROPS ARE ITEM, TITLE, AND ROUTENAME
-        ) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
