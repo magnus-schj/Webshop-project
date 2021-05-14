d305075f4a78488fd08fcd0bc4fa98dbce1008ab
Magnus <beachmagnus+git@gmail.com>
2021-05-14 13:28:36 +0200

    fixed bug

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -44,21 +44,20 @@ const getButtonStyles = (props) => {
 };
 
 export const CustomButtonContainer = styled.button`
-.custom-button {
-    min-width: 165px;
-    width: auto;
-    height: 50px;
-    letter-spacing: 0.5px;
-    line-height: 50px;
-    padding: 0 35px 0 35px;
-    font-size: 15px;
-  
-    text-transform: uppercase;
-    font-family: "Open Sans Condensed";
-    font-weight: bolder;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-  
-    ${getButtonStyles}
-    `;
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: bolder;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+
+  ${getButtonStyles}
+`;
